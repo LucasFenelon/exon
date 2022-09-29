272a6f4ffa813cb07309224c54280d749cb0a165
Lucas Fenelon <lucasgfmoraes@gmail.com>
2022-09-29 13:29:23 -0300

    acerto login

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -13,7 +13,7 @@ import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
 import Router from 'next/router';
-import UserPool from '../pages/userspool';
+import UserPool from './userspool';
 import ExonSignUp from 'src/components/ExonSignUp';
 
 const useStyles = makeStyles((theme) => ({
